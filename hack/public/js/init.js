59e84ee6a8f8642241d48376d3578ca81dda151a
unknown <UC199596@UC199596-TPL-A.ten.thomsonreuters.com>
2015-11-01 03:35:15 +0530

    no message

--- hack/public/js/init.js
+++ hack/public/js/init.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	$("body > div.banner-wrap > div > div > div.rentbutton > input").click(function(){$('.rentbutton').css("background","#FF2300")});
+	$("body > div.banner-wrap > div > div > div.rentbutton > input .rentbutton").click(function(){
+		$('.rentbutton').css("background","#FF2300");$('.byebutton').css("background","#000000");
+	});
+	$('.rentbutton').click(function(){
+		$('.rentbutton').css("background","#FF2300");$('.byebutton').css("background","#000000");
+	});
+	$("body > div.banner-wrap > div > div > div.byebutton > input .byebutton").click(function(){
+		$('.rentbutton').css("background","#000000");$('.byebutton').css("background","#FF2300");
+	});
+	$('.byebutton').click(function(){
+		$('.rentbutton').css("background","#000000");$('.byebutton').css("background","#FF2300");
+	});
 });
