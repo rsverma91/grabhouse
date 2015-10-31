2ba4d8e2136d7eddcc50e1db9d359efd03f3de9d
Shubham Kumar <shubhamdiscover@gmail.com>
2015-10-31 21:06:04 +0530

    New Chnage on http

--- hack/public/controller/mainApp.js
+++ hack/public/controller/mainApp.js
@@ -13,8 +13,19 @@ var myapp = angular.module('mainApp',['ngRoute','mainController','signinupContro
         templateUrl: 'home',
         controller: 'indexCtrl'
 	});
-}]).factory('userDetail', ['', function(){
-	return function name(){
-		
-	};
+}]).factory('userDetail', ['$http', function($http){
+	var hGrabHouseApi = {};
+	hGrabHouseApi.isValidUser = function(userData) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+    hGrabHouseApi.signUpForm = function(userData) {
+      return $http({
+        method: 'JSONP',
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+	return hGrabHouseApi;
 }]);
--- hack/public/controller/signInUpController.js
+++ hack/public/controller/signInUpController.js
@@ -1,11 +1,19 @@
 var signInUp = angular.module('signinupController',[]);
 
 signInUp.controller('signinCtrl', ['$scope', function($scope){
-
+	$scope.signInForm = function(userData){
+	userData.uName,userData.uPwd;
+	userDetail.isValidUser(userData).success(function(response){
+		
+	});
+	}
 }]);
 
 signInUp.controller('signupCtrl', ['$scope','userDetail', function($scope,userDetail){
-	$scope.submitForm = function(userData){
+	$scope.signUpForm = function(userData){
 	userData.uName,userData.uPhn,userData.uEmail,userData.uPwd;
+	userDetail.signUpDetail(userData).success(function(response){
+
+	});
 	}
 }]);
