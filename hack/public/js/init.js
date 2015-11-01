9cc74995a461bd3850452d9d453c6e6d95ce6b74
Raman Saran Verma <raman.verma@redbus.in>
2015-11-01 06:50:47 +0530

    no message

--- hack/public/js/init.js
+++ hack/public/js/init.js
@@ -4,6 +4,4 @@ $(document).ready(function(){
 		$(this).find('input[name="stype"]').prop('checked', true);
 		$(this).css('background-color','#FF2300');
 	});
-	$('body > div.ng-view.ng-scope').addClass('mainSignIn');
-	$('<div class="form-title">Login In</div>').insertBefore('.mainSignIn');
 });
